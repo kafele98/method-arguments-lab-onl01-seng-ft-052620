b5934efcf82f972447c2473579a64067c7f0d9aa
bionic-application-0869 <keller.zoe98@gmail.com>
2020-05-26 19:57:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(a, b)
-  
+def introduction_with_language(name, language)
+  puts "Hi, my name is "
 end
